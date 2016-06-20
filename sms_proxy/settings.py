e49804aefc7233d9f9edaa3b346ad6f3ae80cdb4
adamysu <adamsu@gmail.com>
2016-06-20 12:33:50 -0700

    Update settings.py
    SESSION_START_MSG, SESSION_END_MSG, NO_SESSION_MSG available to be set by environment variables

--- sms_proxy/settings.py
+++ sms_proxy/settings.py
@@ -8,9 +8,9 @@ FLOWROUTE_SECRET_KEY = os.environ.get('FLOWROUTE_SECRET_KEY', None)
 FLOWROUTE_ACCESS_KEY = os.environ.get('FLOWROUTE_ACCESS_KEY', None)
 
 ORG_NAME = os.environ.get('ORG_NAME', 'Your Org Name')
-SESSION_START_MSG = "Your new session has started, send a message!"
-SESSION_END_MSG = "This session has ended, talk to you again soon!"
-NO_SESSION_MSG = "An active session was not found. Please contact support@yourorg.com"
+SESSION_START_MSG = os.environ.get('SESSION_START_MSG', 'Your new session has started, send a message!')
+SESSION_END_MSG = os.environ.get('SESSION_END_MSG', 'This session has ended, talk to you again soon!')
+NO_SESSION_MSG = os.environ.get('NO_SESSION_MSG', 'An active session was not found. Please contact support@yourorg.com')
 
 TEST_DB = "test_sms_proxy.db"
 DB = "sms_proxy.db"
